d76d8ac61d5872cb67c45f8db662f786dd9243de
Rails test course <railscoursetest@gmail.com>
2017-03-08 20:38:15 +0000

    Add user data to article listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
     end
     def create
         #render plain: params[:article].inspect
-        debugger
+        
         @article = Article.new(article_params)
         @article.user = User.first
         if @article.save
