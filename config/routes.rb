66061e3e6bf0fa5b58b43656c51e557ea5ed12aa
Rails test course <railscoursetest@gmail.com>
2017-03-09 01:41:35 +0000

    signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
     root "pages#home"
     get 'about', to: "pages#about"
+    get 'signup', to: "users#new"
+    
     resources :articles
+    
+    # post 'users', to: 'users#create'
+    resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
